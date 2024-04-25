be64093e8f163c143c7d35746de4cac003ab2889
RogerGN <roger.goulart.nascimento@gmail.com>
2024-04-25 15:04:48 -0300

    @feate: route to get event by slug

---
+++ src/routes/get-event-by-slug.ts
@@ -0,0 +1,57 @@
+import { FastifyInstance } from 'fastify'
+import z from 'zod'
+import { prisma } from '../utils/prisma'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+
+const getEventBySlug = async (app: FastifyInstance) => {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/event/:slug',
+    {
+      schema: {
+        params: z.object({
+          slug: z.string(),
+        }),
+        response: {
+          201: z.object({
+            event: z.object({
+              id: z.string().uuid(),
+              title: z.string(),
+              details: z.string().nullable(),
+              maximumAttendees: z.number().int().positive().nullable(),
+              slug: z.string(),
+            }),
+          }),
+          404: z.object({
+            error: z.string(),
+          }),
+          400: z.object({
+            error: z.string(),
+          }),
+        },
+      },
+    },
+    async (req, reply) => {
+      try {
+        const { slug } = req.params
+
+        const event = await prisma.event.findUnique({
+          where: {
+            slug,
+          },
+        })
+
+        if (!event) {
+          return reply.status(404).send({ error: 'Evento não encontrado' })
+        }
+
+        return reply.status(200).send({
+          event,
+        })
+      } catch (error) {
+        return reply.status(400).send({ error: error.message })
+      }
+    },
+  )
+}
+
+export { getEventBySlug }
